a9afbf8ece8f44a711a20fae51a698eb86fab6be
3A632052 <s3A632052@student.ncut.edu.tw>
2019-10-22 19:59:09 +0800

    練習 1：設定 Route 反應 練習 2：設定 Route 接收參數 --> 1. 修改 Route 接受參數 未看到Hello,tom

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//設定 Route 回傳字串
+Route::get('/',function(){
+    return'welcome';
+});
+//設定 Route 回傳 view
+Route::get('/',function(){
+    returnview('welcome');
+});
+//設定 Route 跳轉頁面
+Route::get('/',function(){
+    returnredirect('welcome');
+});
+    Route::get('hello/{name}',function($name){
+        return'Hello,'.$name;
+
+
+    });
